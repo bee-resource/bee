ded0abefef6a22e4fdb413b50934ffe69e833ff7
Jing Chen <jing.chen@crypto.com>
2021-06-12 15:07:18 +0800

    Update install script

--- install_centos.sh
+++ install_centos.sh
@@ -1,6 +1,6 @@
-mkdir bee-node
-mv bee bee-node/
-mv bee.yaml bee-node/
+mkdir /root/bee-node
+mv bee /root/bee-node/
+mv bee.yaml /root/bee-node/
 wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.12/bee-clef_0.4.12_amd64.rpm
 rpm -i bee-clef_0.4.12_amd64.rpm
 chmod +x bee
